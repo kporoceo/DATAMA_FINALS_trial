2bfbf2c6174d128b924fb5f63f06c02a776bf4c6
Kathryn Oroceo <122486076+kporoceo@users.noreply.github.com>
2025-03-05 19:15:44 +0800

    adding test code

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+console.log("Supabase client:", supabase);
+
+
 const supabaseUrl = 'https://zstptnblkfdpjnmvgeng.supabase.co'; // Replace with your Supabase project URL
 const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzdHB0bmJsa2ZkcGpubXZnZW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExNjg0ODYsImV4cCI6MjA1Njc0NDQ4Nn0.q78LYNBD6hApZnR7OpnCz4swAnEJNwx4-sYClwY6SQg.SUPABASE_KEY; // Replace with your Supabase anon key
 
