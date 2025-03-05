5122010a360e9fe1a6ea2f7dd05c56f07e839c14
Kathryn Oroceo <122486076+kporoceo@users.noreply.github.com>
2025-03-05 21:43:42 +0800

    fixfix

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { createClient } from '@supabase/supabase-js';
-
 // Supabase credentials
 const supabaseUrl = 'https://zstptnblkfdpjnmvgeng.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzdHB0bmJsa2ZkcGpubXZnZW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExNjg0ODYsImV4cCI6MjA1Njc0NDQ4Nn0.q78LYNBD6hApZnR7OpnCz4swAnEJNwx4-sYClwY6SQg';
